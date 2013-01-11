69305ef9300ecf798fdbc6d612f70ed33d42be2a
aCasini <kasoale@gmail.com>
2013-01-11 16:25:08 +0100

    Create the implementation of OLSInfo Interface for the OLS service

---
+++ src/ols/src/main/java/it/phoops/geoserver/ols/OLSInfoImpl.java
@@ -0,0 +1,13 @@
+package it.phoops.geoserver.ols;
+
+import org.geoserver.config.impl.ServiceInfoImpl;
+
+/**
+ * 
+ * Default implementation for the {@link OLSInfo} bean.
+ * @author aCasini
+ *
+ */
+public class OLSInfoImpl extends ServiceInfoImpl implements OLSInfo {
+
+}
