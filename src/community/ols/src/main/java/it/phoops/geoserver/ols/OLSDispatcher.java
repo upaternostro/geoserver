1f89a3ed6c8acb5fa88f5dc1387fa1dbc5d0bc0b
upaternostro <ugo.git@paternostro.org>
2013-01-13 18:55:42 +0100

    OLSDispatcher skeleton

---
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/OLSDispatcher.java
@@ -0,0 +1,18 @@
+package it.phoops.geoserver.ols;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class OLSDispatcher extends AbstractController {
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
