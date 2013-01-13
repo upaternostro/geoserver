b70e2960de3d01e5515d14c8cd6add3ca8329d5b
upaternostro <ugo.git@paternostro.org>
2013-01-14 00:38:37 +0100

    Implementing OLS handlers stubs

--- src/community/ols/src/main/java/it/phoops/geoserver/ols/OLSDispatcher.java
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/OLSDispatcher.java
@@ -1,18 +1,69 @@
 package it.phoops.geoserver.ols;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+import org.w3c.dom.Document;
 
 public class OLSDispatcher extends AbstractController {
-
+    private Map<String,OLSHandler>  handlers = new HashMap<String,OLSHandler>();
+    
+    public OLSDispatcher()
+    {
+    }
+    
+    public void setHandlers(Map<String,OLSHandler> handlers)
+    {
+        this.handlers = handlers;
+    }
+    
     @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest request,
-            HttpServletResponse response) throws Exception {
-        // TODO Auto-generated method stub
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpRequest,
+            HttpServletResponse httpResponse) throws Exception {
+        if (METHOD_POST.equals(httpRequest.getMethod())) {
+            DocumentBuilderFactory      domFactory = DocumentBuilderFactory.newInstance();
+            
+            domFactory.setNamespaceAware(true);
+            
+            DocumentBuilder             domBuilder = domFactory.newDocumentBuilder();
+            Document                    domRequest = domBuilder.parse(httpRequest.getInputStream());
+            XPath                       xPath = XPathFactory.newInstance().newXPath();
+            XPathExpression             xPathExpr;
+            
+            for (String path : handlers.keySet()) {
+                xPathExpr = xPath.compile(path);
+                
+                if ((Boolean)xPathExpr.evaluate(domRequest, XPathConstants.BOOLEAN)) {
+                    OLSHandler          handler = handlers.get(path);
+                    Document            domResponse = handler.processRequest(domRequest);
+                    TransformerFactory  transFactory = TransformerFactory.newInstance();
+                    Transformer         transformer = transFactory.newTransformer();
+                    
+//                    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+                    transformer.transform(new DOMSource(domResponse.getFirstChild()),
+                          new StreamResult(httpResponse.getOutputStream()));
+                    break;
+                }
+            }
+        } else {
+            httpResponse.getWriter().println("Invoke Open LS service via POST method!");
+        }
+        
         return null;
     }
-
 }
---
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/GeocodingHandler.java
@@ -0,0 +1,15 @@
+package it.phoops.geoserver.ols.geocoding;
+
+import org.w3c.dom.Document;
+
+import it.phoops.geoserver.ols.OLSHandler;
+
+public class GeocodingHandler implements OLSHandler {
+
+    @Override
+    public Document processRequest(Document request) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
