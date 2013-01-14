ac7253def7eb2a1ac50ec07cc202248f6b06d01c
upaternostro <ugo.git@paternostro.org>
2013-01-14 15:48:51 +0100

    Implementing Open LS service provider plugin framework

---
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/OLSServiceProvider.java
@@ -0,0 +1,5 @@
+package it.phoops.geoserver.ols;
+
+public interface OLSServiceProvider {
+    public abstract String getDescriptionKey();
+}
--- src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/GeocodingHandler.java
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/GeocodingHandler.java
@@ -1,13 +1,26 @@
 package it.phoops.geoserver.ols.geocoding;
 
-import org.w3c.dom.Document;
-
 import it.phoops.geoserver.ols.OLSHandler;
+import it.phoops.geoserver.ols.OLSServiceProvider;
+
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.w3c.dom.Document;
 
 public class GeocodingHandler implements OLSHandler {
 
     @Override
-    public Document processRequest(Document request) {
+    public Document processRequest(ApplicationContext applicationContext, Document request) {
+        Map<String,OLSServiceProvider>  beans = applicationContext.getBeansOfType(OLSServiceProvider.class);
+        OLSServiceProvider              provider;
+        
+        for (String beanName : beans.keySet()) {
+            provider = beans.get(beanName);
+            
+            System.out.println(beanName + ": " + provider);
+        }
+        
         // TODO Auto-generated method stub
         return null;
     }
