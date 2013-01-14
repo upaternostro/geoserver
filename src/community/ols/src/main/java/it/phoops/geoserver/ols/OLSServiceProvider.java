3f52507fa5a16252a96bde775a9a505c32afb760
upaternostro <ugo.git@paternostro.org>
2013-01-14 23:54:15 +0100

    Adding JAXB binding for OpenLS requests

--- src/community/ols/src/main/java/it/phoops/geoserver/ols/OLSServiceProvider.java
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/OLSServiceProvider.java
@@ -1,5 +1,7 @@
 package it.phoops.geoserver.ols;
 
+import net.opengis.www.xls.GeocodeRequestType;
+
 public interface OLSServiceProvider {
     public abstract String getDescriptionKey();
 }
--- src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/GeocodingServiceProvider.java
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/GeocodingServiceProvider.java
@@ -1,7 +1,11 @@
 package it.phoops.geoserver.ols.geocoding;
 
+import net.opengis.www.xls.GeocodeRequestType;
+import net.opengis.www.xls.GeocodeResponseType;
 import it.phoops.geoserver.ols.OLSServiceProvider;
 
 public interface GeocodingServiceProvider extends OLSServiceProvider {
+
+    public abstract GeocodeResponseType geocode(GeocodeRequestType input);
 
 }
