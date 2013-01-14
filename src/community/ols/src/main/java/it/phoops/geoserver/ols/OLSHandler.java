63c9419aa972b95e17ac0411c165acc207dec6ee
upaternostro <ugo.git@paternostro.org>
2013-01-14 23:54:15 +0100

    Adding JAXB binding for OpenLS requests

--- src/community/ols/src/main/java/it/phoops/geoserver/ols/OLSHandler.java
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/OLSHandler.java
@@ -4,5 +4,5 @@ import org.springframework.context.ApplicationContext;
 import org.w3c.dom.Document;
 
 public interface OLSHandler {
-    public abstract Document processRequest(ApplicationContext applicationContext, Document request);
+    public abstract Document processRequest(ApplicationContext applicationContext, Document request) throws OLSException;
 }
--- src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/ReverseGeocodingHandler.java
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/ReverseGeocodingHandler.java
@@ -1,5 +1,6 @@
 package it.phoops.geoserver.ols.geocoding;
 
+import it.phoops.geoserver.ols.OLSException;
 import it.phoops.geoserver.ols.OLSHandler;
 
 import org.springframework.context.ApplicationContext;
@@ -8,7 +9,7 @@ import org.w3c.dom.Document;
 public class ReverseGeocodingHandler implements OLSHandler {
 
     @Override
-    public Document processRequest(ApplicationContext applicationContext, Document request) {
+    public Document processRequest(ApplicationContext applicationContext, Document request) throws OLSException {
         // TODO Auto-generated method stub
         return null;
     }
