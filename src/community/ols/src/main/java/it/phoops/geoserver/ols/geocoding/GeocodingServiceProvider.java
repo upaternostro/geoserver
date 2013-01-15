7d6b6a125df2173eb6c0827c397cbab2695836f2
upaternostro <ugo.git@paternostro.org>
2013-01-15 18:57:21 +0100

    Adding first implementation of RFC59 web service client

--- src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/GeocodingServiceProvider.java
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/GeocodingServiceProvider.java
@@ -4,10 +4,9 @@ import javax.xml.bind.JAXBElement;
 
 import net.opengis.www.xls.GeocodeRequestType;
 import net.opengis.www.xls.GeocodeResponseType;
+import it.phoops.geoserver.ols.OLSException;
 import it.phoops.geoserver.ols.OLSServiceProvider;
 
 public interface GeocodingServiceProvider extends OLSServiceProvider {
-
-    public abstract JAXBElement<GeocodeResponseType> geocode(GeocodeRequestType input);
-
+    public abstract JAXBElement<GeocodeResponseType> geocode(GeocodeRequestType input) throws OLSException;
 }
