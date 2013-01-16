5ca17f4875bde5ecdedae1e8c23482fc813ce1f4
aCasini <kasoale@gmail.com>
2013-01-16 16:54:03 +0100

    Changes administration page to manage services ols

--- src/community/ols_web/src/main/java/it/phoops/geoserver/ols/web/OLSAdminPage.java
+++ src/community/ols_web/src/main/java/it/phoops/geoserver/ols/web/OLSAdminPage.java
@@ -1,20 +1,28 @@
 package it.phoops.geoserver.ols.web;
 
 import it.phoops.geoserver.ols.OLSInfo;
+import it.phoops.geoserver.ols.geocoding.GeocodingServiceProvider;
+import it.phoops.geoserver.ols.util.ApplicationContextUtil;
+import it.phoops.geoserver.ols.web.component.ServiceDropDownChoice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel;
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.ListChoice;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.geoserver.web.services.BaseServiceAdminPage;
+import org.springframework.context.ApplicationContext;
 
 /**
  * 
@@ -23,53 +31,76 @@ import org.geoserver.web.services.BaseServiceAdminPage;
  */
 public class OLSAdminPage extends BaseServiceAdminPage<OLSInfo> {
 	
-	private List<ITab> tabsOLS = null;
-	private String urlRFC59;
-	private String timeoutRFC59;
+	private List<ITab> 				tabsOLS = null;
+	private List<String> 			SERVICES = Arrays.asList(new String[] {"Geoconding", "Reverse Geocoding", "Routing Navigation"});
+	private TabbedPanel				tabPanelOLS = null;
+	private String 					selectedService = null;
+	
+	private String 					urlRFC59;
+	private String 					timeoutRFC59;
 	
     @Override
     protected void build(IModel info, Form form) {
-        // TODO Auto-generated method stub
-    	tabsOLS = new ArrayList<ITab>();
-        //Add the tab RFC59
-    	tabsOLS.add(new AbstractTab(new Model<String>("RFC59")) {
-			
-			@Override
-			public Panel getPanel(String panelId) {
-				return new RFCP59Panel(panelId);
-			}
-		});
         
-    	//Add the tab SOLR
-    	tabsOLS.add(new AbstractTab(new Model<String>("SOLR")) {
-			
-			@Override
-			public Panel getPanel(String panelId) {
-				return new SOLRPanel(panelId);
+        SERVICES = new ArrayList<String>();
+        SERVICES.add("Geocoding");
+        SERVICES.add("Reverse Geocoding");
+        SERVICES.add("Routing Navigation");
+        
+        ServiceDropDownChoice listServices = new ServiceDropDownChoice("service", new PropertyModel<String>(this, "selectedService"), SERVICES, form);
+		form.add(listServices);
+
+		if(listServices.getSelectedService() == null){
+			//Do nothing - non visualizzare il tab panel
+			if(tabsOLS == null){
+				tabsOLS = new ArrayList<ITab>();
 			}
-		});
-    	
-    	//Add the tab OTP
-    	tabsOLS.add(new AbstractTab(new Model<String>("OTP")) {
-			
-			@Override
-			public Panel getPanel(String panelId) {
-				return new OTPPanel(panelId);
-			}
-		});
-    	
-        form.add(new TabbedPanel("tabList", tabsOLS));
+			tabPanelOLS = new TabbedPanel("tabList", tabsOLS);
+			tabPanelOLS.setVisible(false);
+			form.add(tabPanelOLS);
+		}else{
+		
+	    	tabsOLS = new ArrayList<ITab>();
+	        //Add the tab RFC59
+	    	
+	    	tabsOLS.add(new AbstractTab(new Model<String>("RFC59")) {
+				
+				@Override
+				public Panel getPanel(String panelId) {
+					return new RFCP59Panel(panelId);
+				}
+			});
+	        
+	    	//Add the tab SOLR
+	    	tabsOLS.add(new AbstractTab(new Model<String>("SOLR")) {
+				
+				@Override
+				public Panel getPanel(String panelId) {
+					return new SOLRPanel(panelId);
+				}
+			});
+	    	
+	    	//Add the tab OTP
+	    	tabsOLS.add(new AbstractTab(new Model<String>("OTP")) {
+				
+				@Override
+				public Panel getPanel(String panelId) {
+					return new OTPPanel(panelId);
+				}
+			});
+	    	
+	    	form.remove(tabPanelOLS);
+	        form.add(new TabbedPanel("tabList", tabsOLS));
+		}
     }
 
     @Override
     protected Class<OLSInfo> getServiceClass() {
-        // TODO Da sostituire con OLSInfo
         return OLSInfo.class;
     }
 
     @Override
     protected String getServiceName() {
-        // TODO Da sostituire con OLS
         return "OLS";
     }
     
@@ -139,7 +170,22 @@ public class OLSAdminPage extends BaseServiceAdminPage<OLSInfo> {
 
 	public void setTimeoutRFC59(String timeoutRFC59) {
 		this.timeoutRFC59 = timeoutRFC59;
-	};
-    
-    
+	}
+
+	public TabbedPanel getTabPanelOLS() {
+		return tabPanelOLS;
+	}
+
+	public void setTabPanelOLS(TabbedPanel tabPanelOLS) {
+		this.tabPanelOLS = tabPanelOLS;
+	}
+
+	public String getSelectedService() {
+		return selectedService;
+	}
+
+	public void setSelectedService(String selectedService) {
+		this.selectedService = selectedService;
+	}
+	
 }
