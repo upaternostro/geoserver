a14d4eb16d2d85786011bbcdc1db14f4b48b7a49
aCasini <kasoale@gmail.com>
2013-01-18 10:30:41 +0100

    Adding OLSService enum

--- src/community/ols/src/main/java/it/phoops/geoserver/ols/OLSService.java
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/OLSService.java
@@ -1,7 +1,18 @@
 package it.phoops.geoserver.ols;
 
 public enum OLSService {
-    GEOCODING,
-    REVERSE_GEOCODING,
-    ROUTING_NAVIGATION
+    GEOCODING("1"),
+    REVERSE_GEOCODING("2"),
+    ROUTING_NAVIGATION("3");
+    
+    private String code;
+    
+    OLSService(String code) {
+    	this.code = code;
+    }
+    
+    @Override
+    public String toString() {
+    	return code;
+    }
 }
