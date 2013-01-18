d6c4f947467e5f65134a97b075818e5bfdb68162
aCasini <kasoale@gmail.com>
2013-01-18 10:20:50 +0100

    Adding the dropDownChoice for algorithm

---
+++ src/community/ols-RFC59/src/main/java/it/phoops/geoserver/ols/geocoding/component/AlgorithmDropDownChoise.java
@@ -0,0 +1,33 @@
+package it.phoops.geoserver.ols.geocoding.component;
+
+import it.phoops.geoserver.ols.geocoding.component.RFC59Tab.OLSAlgorithmType;
+
+import java.util.List;
+
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.model.PropertyModel;
+
+public class AlgorithmDropDownChoise extends DropDownChoice<OLSAlgorithmType>{
+	private OLSAlgorithmType selectedAlgorithm;
+
+	public AlgorithmDropDownChoise(String id, PropertyModel<OLSAlgorithmType> model, List<OLSAlgorithmType> displayData) {
+			super(id,model,displayData);
+	}
+	@Override
+	protected void onSelectionChanged(OLSAlgorithmType newSelection) {
+		// TODO Auto-generated method stub
+		super.onSelectionChanged(newSelection);
+	}
+	
+	@Override
+	protected boolean wantOnSelectionChangedNotifications() {
+		return true;
+	}
+	public OLSAlgorithmType getSelectedAlgorithm() {
+		return selectedAlgorithm;
+	}
+	public void setSelectedAlgorithm(OLSAlgorithmType selectedAlgorithm) {
+		this.selectedAlgorithm = selectedAlgorithm;
+	}
+
+}
