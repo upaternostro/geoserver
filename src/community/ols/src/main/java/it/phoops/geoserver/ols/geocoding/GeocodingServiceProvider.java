ffeb1fae9e2a00580f8f86912bd895a4fbe10c61
upaternostro <ugo.git@paternostro.org>
2013-01-22 00:52:29 +0100

    Adding configuration event listeners

--- src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/GeocodingServiceProvider.java
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/GeocodingServiceProvider.java
@@ -1,13 +1,13 @@
 package it.phoops.geoserver.ols.geocoding;
 
 import it.phoops.geoserver.ols.OLSException;
-import it.phoops.geoserver.ols.OLSServiceProviderGUI;
+import it.phoops.geoserver.ols.OLSServiceProvider;
 
 import javax.xml.bind.JAXBElement;
 
 import net.opengis.www.xls.GeocodeRequestType;
 import net.opengis.www.xls.GeocodeResponseType;
 
-public interface GeocodingServiceProvider extends OLSServiceProviderGUI {
+public interface GeocodingServiceProvider extends OLSServiceProvider {
     public abstract JAXBElement<GeocodeResponseType> geocode(GeocodeRequestType input) throws OLSException;
 }
--- src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/ReverseGeocodingServiceProvider.java
+++ src/community/ols/src/main/java/it/phoops/geoserver/ols/geocoding/ReverseGeocodingServiceProvider.java
@@ -1,7 +1,7 @@
 package it.phoops.geoserver.ols.geocoding;
 
-import it.phoops.geoserver.ols.OLSServiceProviderGUI;
+import it.phoops.geoserver.ols.OLSServiceProvider;
 
-public interface ReverseGeocodingServiceProvider extends OLSServiceProviderGUI {
+public interface ReverseGeocodingServiceProvider extends OLSServiceProvider {
 
 }
